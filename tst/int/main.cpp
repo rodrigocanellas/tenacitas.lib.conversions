03f2feaf7967e99af3667aed01958c2df9ec0b99
Rodrigo Canellas <rodrigo.canellas@gmail.com>
2023-03-21 19:57:14 -0300

    improving integer conversion

--- tst/int/main.cpp
+++ tst/int/main.cpp
@@ -3,7 +3,7 @@
 #include <limits>
 #include <optional>
 
-#include <tenacitas.lib.conversions/alg/int.h>
+#include <tenacitas.lib.conversions/alg/cvt.h>
 #include <tenacitas.lib.program/alg/options.h>
 #include <tenacitas.lib.test/alg/tester.h>
 
@@ -17,15 +17,15 @@ struct test000 {
 
     using namespace alg::internal;
 
-    auto _maybe{to_int<uint8_t>("256")};
+    std::optional<uint8_t> _maybe{to_int<uint8_t>("256")};
 
     if (_maybe) {
-      std::cout << "It should not have converted, but it did to "
+      std::cerr << "It should not have converted, but it did to "
                 << _maybe.value() << '\n';
       return false;
     }
 
-    std::cout << "It did not convert, as expected\n";
+    std::cerr << "It did not convert, as expected\n";
     return true;
   }
 };
@@ -40,21 +40,13 @@ struct test001 {
     auto _maybe{to_int<uint8_t>("345")};
 
     if (_maybe) {
-      std::cout << "It should not have converted, but it did to "
+      std::cerr << "It should not have converted, but it did to "
                 << _maybe.value() << '\n';
       return false;
     }
 
-    std::cout << "It did not convert, as expected\n";
+    std::cerr << "It did not convert, as expected\n";
 
-    //    std::cout << "could not convert, as it should not" << std::endl;
-    //    if (!_maybe) {
-    //      std::cout << "should have converted, but it did not\n";
-    //      return false;
-    //    }
-
-    //    std::cout << "i = " << static_cast<int16_t>(_maybe.value()) <<
-    //    std::endl;
     return true;
   }
 };
@@ -69,19 +61,40 @@ struct test002 {
     auto _maybe{to_int<uint8_t>("255")};
 
     if (!_maybe) {
-      std::cout << "It did convert, but it should have\n";
+      std::cerr << "It did convert, but it should have\n";
       return false;
     }
 
     auto _i{std::move(_maybe.value())};
     if (_i != 255) {
-      std::cout << "It converted to " << static_cast<uint16_t>(_i)
+      std::cerr << "It converted to " << static_cast<uint16_t>(_i)
                 << ", but it should have converted to 255z\n";
       return false;
     }
 
-    std::cout << "It converted to " << static_cast<int16_t>(_maybe.value())
+    std::cerr << "It converted to " << static_cast<int16_t>(_maybe.value())
               << ", as expected\n";
+
+    return true;
+  }
+};
+
+struct test003 {
+  static std::string desc() { return "internal::to_int<uint8_t>(\"1234\")"; }
+
+  bool operator()(const program::alg::options &) {
+
+    using namespace alg::internal;
+
+    auto _maybe{to_int<uint8_t>("1234")};
+
+    if (_maybe) {
+      std::cerr << "It should not have converted, but it did to "
+                << _maybe.value() << '\n';
+      return false;
+    }
+
+    std::cerr << "It did not convert, as expected\n";
 
     return true;
   }
@@ -93,4 +106,5 @@ int main(int argc, char **argv) {
   run_test(_test, test000);
   run_test(_test, test001);
   run_test(_test, test002);
+  run_test(_test, test003);
 }
